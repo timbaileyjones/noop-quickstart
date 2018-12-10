726f2a0b31b0522a3e67f5651f04a0fdfe181d83
Adam Daugherty <adamdaugherty@users.noreply.github.com>
2018-12-10 18:55:23 -0500

    Fixed an issue in script ordering.

--- ui/index.js
+++ ui/index.js
@@ -6,13 +6,13 @@ app.use(Express.json())
 app.get('*', (req, res) => {
   res.send(`
     <head>
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
       <title>noop Sample App</title>
 

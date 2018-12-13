29d608b3b58a231b8bd59daae9b2e146130ac1a1
Milin Patel <milin.patel@rearc.io>
2018-12-13 12:23:30 -0500

    Minor fix to ui

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './app.vue'
+import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
